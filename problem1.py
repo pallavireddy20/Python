b9dddfbd2edce5c953c32921cc934fc6b0403613
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-12-06 14:55:32 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,26 @@
+#Create three variables: name, age, college. Print them in one line.
+name="pallavi"
+age=20
+college= "St.peters engineering college"
+print("my name is:",name, "my age is:",age ,"my college name is:",college)
+
+#Swap two variables without using a third variable
+a=10
+b=20
+[a,b]=[b,a]
+print(a,b)
+
+#Create a variable x = 10. Update it to x = x + 5. Print the result.
+x=10
+x+=5
+print(x)
+
+#Store your favourite actor name in a variable and print it.
+hero="prabhas"
+print("My favorite hero is:",hero)
+
+#Create a variable for temperature in Celsius. Convert it to Fahrenheit
+C=25
+F=(C*9/5)+32
+print(F)
+
---
+++ problem2.py
@@ -0,0 +1,32 @@
+#What is the data type of 45.89? Write code to check it.
+x=45.89
+print(type(x))
+
+#Convert the integer 100 into:string,float
+y=100
+a=float(y)
+b=str(y)
+print(a)
+print(b)
+
+#Convert "123.45" into a float.
+z=123.45
+p=float(123.45)
+print(p)
+
+#Create a list of 5 fruits and print the second fruit.
+fruits=["apple","grapes","mango","banana","orange"]
+secondfruit=fruits[1]
+print(secondfruit)
+
+#Write a program to calculate the area of a circle (radius given)
+radius =5
+areaofcircle= 3.14*radius*radius
+print(areaofcircle)
+
+#Check if a number is even or odd using % operator.
+num=8
+if(num%2==0):
+    print("Evennumber")
+else:
+    print("Oddnumber")
