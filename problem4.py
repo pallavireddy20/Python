c8f7d23d971463e50e65f13f821ccb42cb0ff76d
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-12-07 15:46:19 +0530

    Add files via upload

---
+++ problem4.py
@@ -0,0 +1,57 @@
+#Write a program to find the remainder when 89 is divided by 7.
+
+a = 89
+b=7
+modulo = a%b
+print(modulo)
+
+#Check if 25 is greater than 18 using comparison operator
+a=25
+b=18
+if(a>b):
+    print(True)
+else:
+    print(False)
+
+   # Given a = 12 and b = 5, find:a + b,a - b,a * b,a / b,a % b
+a=12
+b=5
+print("Addition:", a+b,"Subtraction:", a-b,"Multiplication:", a*b,"Division:" ,a/b,"madulo:",a%b)
+
+#Take x = 5, convert it to float, then multiply by 2
+x=5
+num=float(x)
+num1=num*2
+print(num1)
+
+#Using 3 variables a, b, c — print their average.
+x=10
+y=10
+z=10
+avg=(x+y+z)/3
+print(avg)
+
+#Convert "50" to an integer and add 20.
+
+x="50"
+num=int(x)
+num1=num+20
+print(num1)
+
+#Write a program that takes a number and checks if it is positive, negative, or zero.
+a=-6
+if(a>=0):
+    print("Positive")
+elif(a<0):
+    print("Negative")
+else:
+    print("zero")
+
+#Create two numbers and print which one is larger
+a=50
+b=60
+if a>b:
+    print("a is larger:",a)
+else:
+    print("b is larger:",b)
+
