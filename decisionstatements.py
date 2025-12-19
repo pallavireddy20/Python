8c4543b90e39355f7ca6a41a45935e32b720bdde
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-12-19 20:52:24 +0530

    Add files via upload

---
+++ decisionstatements.py
@@ -0,0 +1,26 @@
+#w="summer"
+#if w=="sunny":
+    #print("Cricket")
+#elif w=="rainy":
+    #print("games")
+#else:
+    #print("out door games")
+
+    # Nested if statement
+weather="sunny"
+day="night"
+if weather=="sunny":
+        if day=="day":
+            print("play with toy")
+        else:
+            print("its nyt time to sleep")
+elif weather=="rainy":
+        print("you ply with toy")
+elif weather=="sunny":
+        if day=="night":
+            print("play with teddy bear")
+        else:
+            print("play with snowman toy")
+else:
+        print("read a story book")
+        
